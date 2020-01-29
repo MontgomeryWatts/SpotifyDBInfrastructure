e64ceedf328fd6fa6f3b4ed1c5c8e2a6c3e985b7
Montgomery Watts <mjw4633@rit.edu>
2020-01-28 19:18:31 -0500

    The lambda_arn output isn't needed because the datalake is internal to the import service

--- src/microservices/import/src/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "producer_arns" {
-  value       = ["${module.import-entity-lambda.lambda_role_arn}"]
-  description = "The ARNs of resources that should be granted PUT access to the datalake"
-}
