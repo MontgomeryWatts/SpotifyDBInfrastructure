4860a9615190287f8850c8cff9161cc8b62f2608
Montgomery Watts <mjw4633@rit.edu>
2020-08-07 14:14:15 -0400

    EC2 time

--- src/main.tf
+++ src/main.tf
@@ -22,14 +22,3 @@ terraform {
 data "aws_caller_identity" "identity" {
 
 }
-
-data "aws_iam_policy_document" "lambda_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
--- src/mongodb_atlas.tf
+++ src/mongodb_atlas.tf
@@ -1,11 +1,16 @@
-resource "mongodbatlas_project" "mongodb_atlas_randomizer_project" {
-  name   = "spotifydb-randomizer"
+locals {
+  # aws_region looks like us-east-1, MongoDB Atlas wants US_EAST_1
+  mongodb_atlas_region = replace(upper(var.aws_region), "-", "_")
+}
+
+resource "mongodbatlas_project" "mongodb_atlas_sampler_project" {
+  name   = "sampler-for-spotify"
   org_id = var.mongodb_atlas_organization_id
 }
 
-resource "mongodbatlas_cluster" "mongodb_atlas_playlist_cluster" {
-  name       = "spotifydb-playlist-cluster"
-  project_id = mongodbatlas_project.mongodb_atlas_randomizer_project.id
+resource "mongodbatlas_cluster" "sampler_for_spotify_cluster" {
+  name       = "sampler-for-spotify-cluster"
+  project_id = mongodbatlas_project.mongodb_atlas_sampler_project.id
 
   mongo_db_major_version       = "4.0"
   disk_size_gb                 = 2
@@ -13,51 +18,40 @@ resource "mongodbatlas_cluster" "mongodb_atlas_playlist_cluster" {
 
   provider_name               = "AWS"
   provider_instance_size_name = "M10"
-  provider_region_name        = var.aws_region
-
-  lifecycle { # Can't modify M2 instances in place
-    ignore_changes = [
-      provider_region_name, # Atlas formats us-east-1 as US_EAST_1 and terraform tries to change it
-    ]
-  }
+  provider_region_name        = local.mongodb_atlas_region
 }
 
-resource "mongodbatlas_database_user" "producer_user" {
-  username           = "lambda_producer"
+resource "mongodbatlas_database_user" "read_only_user" {
+  username           = "read_only_user"
   password           = "CHANGEMANUALLYINATLAS"
-  project_id         = mongodbatlas_project.mongodb_atlas_randomizer_project.id
+  project_id         = mongodbatlas_project.mongodb_atlas_sampler_project.id
   auth_database_name = "admin"
 
   roles {
-    role_name     = "readWrite"
-    database_name = "music"
+    role_name     = "read"
+    database_name = "spotify_sampler"
   }
 }
 
-resource "mongodbatlas_network_container" "spotifydb_playlist_network_container" { # Synonymous with VPC
-  project_id       = mongodbatlas_project.mongodb_atlas_randomizer_project.id
+resource "mongodbatlas_network_container" "sampler_network_container" {
+  project_id       = mongodbatlas_project.mongodb_atlas_sampler_project.id
   provider_name    = "AWS"
   atlas_cidr_block = "192.168.0.0/21"
-  region_name      = var.aws_region
-
-  lifecycle {
-    ignore_changes = [
-      region_name, # Atlas formats us-east-1 as US_EAST_1 and terraform tries to change it
-    ]
-  }
+  region_name      = local.mongodb_atlas_region
 }
 
-resource "mongodbatlas_network_peering" "mongodbatlas_to_playlist_vpc_peering" {
-  accepter_region_name   = var.aws_region
-  project_id             = mongodbatlas_project.mongodb_atlas_randomizer_project.id
-  container_id           = mongodbatlas_network_container.spotifydb_playlist_network_container.container_id
+resource "mongodbatlas_network_peering" "mongodbatlas_to_ec2_vpc_peering" {
+  accepter_region_name   = local.mongodb_atlas_region
+  project_id             = mongodbatlas_project.mongodb_atlas_sampler_project.id
+  container_id           = mongodbatlas_network_container.sampler_network_container.container_id
   provider_name          = "AWS"
-  vpc_id                 = aws_vpc.playlist_lambda_to_mongodbatlas_vpc.id
+  vpc_id                 = aws_vpc.ec2_to_mongodbatlas_vpc.id
   aws_account_id         = data.aws_caller_identity.identity.account_id
-  route_table_cidr_block = aws_vpc.playlist_lambda_to_mongodbatlas_vpc.cidr_block
+  route_table_cidr_block = aws_vpc.ec2_to_mongodbatlas_vpc.cidr_block
 }
 
 resource "mongodbatlas_project_ip_whitelist" "mongodb_whitelist" {
-  project_id         = mongodbatlas_project.mongodb_atlas_randomizer_project.id
-  aws_security_group = aws_security_group.mongodb_playlist_lambda_security_group.id
+  project_id         = mongodbatlas_project.mongodb_atlas_sampler_project.id
+  aws_security_group = aws_security_group.ec2_to_mongodbatlas_sg.id
+  depends_on = [mongodbatlas_network_peering.mongodbatlas_to_ec2_vpc_peering]
 }
