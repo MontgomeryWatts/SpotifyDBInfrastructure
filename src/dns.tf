3fa5b0468bb9695b9ec3b77c645524ab0200b9b8
Montgomery Watts <mjw4633@rit.edu>
2020-08-15 18:35:01 -0400

    Point website alias records to load balancer

--- src/dns.tf
+++ src/dns.tf
@@ -16,6 +16,30 @@ resource "aws_route53_record" "spotifydb_cert_record" {
   ttl     = 300
 }
 
+resource "aws_route53_record" "apex_record" {
+  zone_id = aws_route53_zone.spotifydb_hosted_zone.zone_id
+  name    = var.site_url
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.application_load_balancer.dns_name
+    zone_id                = aws_lb.application_load_balancer.zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "www_record" {
+  zone_id = aws_route53_zone.spotifydb_hosted_zone.zone_id
+  name    = "www.${var.site_url}"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.application_load_balancer.dns_name
+    zone_id                = aws_lb.application_load_balancer.zone_id
+    evaluate_target_health = false
+  }
+}
+
 resource "aws_acm_certificate_validation" "spotifydb_cert_validation" {
   certificate_arn         = aws_acm_certificate.spotifydb_cert.arn
   validation_record_fqdns = [aws_route53_record.spotifydb_cert_record.fqdn]
