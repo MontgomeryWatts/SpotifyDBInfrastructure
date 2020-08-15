ce56730eedf5b172e15aa877b58182ac84cd6d27
Montgomery Watts <mjw4633@rit.edu>
2020-08-15 19:53:53 -0400

    Add certificate to ALB

--- src/alb.tf
+++ src/alb.tf
@@ -20,6 +20,24 @@ resource "aws_lb_listener" "load_balancer_http_listener" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "load_balancer_https_listener" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.spotifydb_cert.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_server_target_group.arn
   }
