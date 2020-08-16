d156731e333f38fad24b1dfdf5d5745dbfd101cd
Montgomery Watts <mjw4633@rit.edu>
2020-08-16 14:04:30 -0400

    HTTPS traffic not needed between load balancer and ECS

--- src/vpc.tf
+++ src/vpc.tf
@@ -77,28 +77,10 @@ resource "aws_security_group_rule" "load_balancer_http_web_traffic_outbound_rule
   source_security_group_id = aws_security_group.container_sg.id
 }
 
-resource "aws_security_group_rule" "load_balancer_https_web_traffic_outbound_rule" {
-  type                     = "egress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.load_balancer_sg.id
-  source_security_group_id = aws_security_group.container_sg.id
-}
-
 resource "aws_security_group_rule" "container_http_web_traffic_inbound_rule" {
   type                     = "ingress"
   from_port                = 80
   to_port                  = 80
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.container_sg.id
-  source_security_group_id = aws_security_group.load_balancer_sg.id
-}
-
-resource "aws_security_group_rule" "container_https_web_traffic_inbound_rule" {
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
   protocol                 = "tcp"
   security_group_id        = aws_security_group.container_sg.id
   source_security_group_id = aws_security_group.load_balancer_sg.id
